7e0221d1d2d7efdfdb0b6f4963801f5aa968ae82
Riki Montgomery <mostriki820@gmail.com>
2017-11-27 19:15:54 -0800

    play with slice

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 var mySentence = prompt("write a sentence."); //comes back from prompt input: Toast Output TT
+console.log(mySentence)
 
 var first = mySentence.charAt(0); //first char, T
+console.log(first);
 var last = mySentence.charAt(mySentence.length-1); //0 based index, 5 -1 =4, T
+console.log(last);
 
-
-function newString(){
-  var someVariable = "hello";
- return first.toUpperCase() + last.toUpperCase()
+function middle(){
+  return mySentence.slice(1, 4);
 }
 
-alert (newString());
+function newString(){
+ return(first.toUpperCase() + last.toUpperCase());
+}
+alert(newString(mySentence));
 
 function reverseString (){
   var firstAndLast = newString();
   return firstAndLast.charAt(1) + firstAndLast.charAt(0);
 }
 
-alert (reverseString());
+function solution(){
+  var x = reverseString(mySentence)
 
+}
 
-alert (newString();
+alert(reverseString(mySentence) + middle(mySentence));
+
+// function newString(mySentence){
+//   var first = mySentence.charAt(0);
+//   var last = mySentence.charAt(mySentence.length-1);
+//   var solution = first.toUpperCase() + last.toUpperCase();
+//   return solution.split("").reverse().join("");
+// }
 
 // alert (mySentence + newString);
 
